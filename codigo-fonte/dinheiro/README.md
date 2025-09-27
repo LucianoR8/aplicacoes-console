290bc4888368cb505f80efc2be9714e26b3684e8
LucianoR8 <luciribsantos@gmail.com>
2025-09-27 15:39:02 -0300

    Sincronizacao de endereco remoto

---
+++ codigo-fonte/dinheiro/README.md
@@ -0,0 +1,41 @@
+# Separador de dinheiro
+
+## Sobre:
+
+A aplicação em desenvolvimento tem como objetivo separar eum valor informado pelo usuário em cédulas e moedas, facilitando o troco ou organização de valores. Útil em situações do dia a dia em que exigem cálculo rápido.
+
+### Tecnologias utilizadas:
+
+ - C#
+ - .NET 8.0
+ - Console Application
+
+### Como funciona:
+
+***Entrada de dados***
+
+[Imagem]
+
+***Processamento de Dados***
+
+[explicação]
+
+***Saída de Dados***
+
+[imagem]
+
+### Execução:
+Após clonar esse repositório, abra o editor de código(sugestão: Visual Studio 2022) e carregue o arquivo 'ContadorDinheiro.sln' como uma solução.
+
+Para executar a aplicação sem depuração, pressione as teclas "Ctrl+F5" e a aplicação estará em funcionamento.
+
+### Código-Fonte:
+
+ - [Separador de dinheiro](https://github.com/LucianoR8/separador-dinheiro/blob/master/csharp/ContadorDinheiro/ContadorDinheiro/Program.cs)
+
+### Melhorias futuras:
+
+ - Adaptar o código em formato web, assim tornando o uso da aplicação mais intuitiva.
+
+---
+Feito por [Luciano Ribeiro](https://github.com/LucianoR8)
---
+++ codigo-fonte/dinheiro/csharp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double valor;
+            int nota, moeda, resto, resultado;
+
+            Console.Write("Informe o valor que você quer separar: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            resto = (int)(valor * 100 + 0.05);
+
+
+
+            Console.WriteLine("Notas:");
+
+            nota = 100;
+            resultado = resto / (nota * 100);
+            Console.WriteLine(resultado + " nota(s) de R$100.00");
+            resto = resto % (nota * 100);
+
+            nota = 50;
+            resultado = resto / (nota * 100);
+            Console.WriteLine(resultado + " nota(s) de R$50.00");
+            resto = resto % (nota * 100);
+
+            nota = 20;
+            resultado = resto / (nota * 100);
+            Console.WriteLine(resultado + " nota(s) de R$20.00");
+            resto = resto % (nota * 100);
+
+            nota = 10;
+            resultado = resto / (nota * 100);
+            Console.WriteLine(resultado + " nota(s) de R$10.00");
+            resto = resto % (nota * 100);
+
+            nota = 5;
+            resultado = resto / (nota * 100);
+            Console.WriteLine(resultado + " nota(s) de R$5.00");
+            resto = resto % (nota * 100);
+
+            nota = 2;
+            resultado = resto / (nota * 100);
+            Console.WriteLine(resultado + " nota(s) de R$2.00");
+            resto = resto % (nota * 100);
+
+            Console.WriteLine("Moedas:");
+
+            moeda = 100;
+            resultado = resto / moeda;
+            Console.WriteLine(resultado + " moeda(s) de 1 real");
+            resto = resto % moeda;
+
+            moeda = 50;
+            resultado = resto / moeda;
+            Console.WriteLine(resultado + " moeda(s) de cinquenta centavos");
+            resto = resto % moeda;
+
+            moeda = 25;
+            resultado = resto / moeda;
+            Console.WriteLine(resultado + " moeda(s) de vinte e cinco centavos");
+            resto = resto % moeda;
+
+            moeda = 10;
+            resultado = resto / moeda;
+            Console.WriteLine(resultado + " moeda(s) de 10 centavos");
+            resto = resto % moeda;
+
+            moeda = 5;
+            resultado = resto / moeda;
+            Console.WriteLine(resultado + " moeda(s) de 5 centavos");
+            resto = resto % moeda;
+
+
+            Console.WriteLine("Faltaram: " + resto + " centavos para completar o valor");
+
+
+
+
+
+        }
+    }
+}
